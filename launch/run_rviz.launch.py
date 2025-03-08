8556ab54cc12209f937d0a1ef4cc4013b2a933dd
Onicc <xinchen.cai@hotmail.com>
2025-03-08 02:41:26 +0000

    Update mid360 configuration and add mid360_sparse configuration for improved lidar and IMU topic handling; introduce launch files for RViz integration

---
+++ launch/run_rviz.launch.py
@@ -0,0 +1,22 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration, Command
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+
+    share_dir = get_package_share_directory('liorf')
+    rviz_config_file = os.path.join(share_dir, 'rviz', 'mapping.rviz')
+
+    return LaunchDescription([
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            arguments=['-d', rviz_config_file],
+            output='screen'
+        )
+    ])
